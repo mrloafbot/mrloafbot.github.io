657cf32fc6eb36ebe213e7c258792fdebdba247c
dfulmer <dfulmer@katanaus.com>
2024-01-09 18:05:15 -0500

    clean new post.py, remove output

--- content/post/2024-01-09__the fart of ages/the_fart_of_ages.md
+++
@@ -1,7 +0,0 @@
-
-+++
-title = 'the fart of ages'
-date = 2024-01-09
-draft = false
-tags = []
-+++
--- newpost.py
+++ newpost.py
@@ -4,29 +4,10 @@ import os
 import sys
 from datetime import date
 
+## Gather Variables
 script_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
-
 newPostPath = os.path.join( script_directory, 'content', 'post')
-
-## get name from input
-
-postName = input('Name of Post\n')
-
-postFileName = postName.replace(" ", "_")
-
-## remove any spaces
-
 today = str(date.today())
-
-postFolderName = today+"__"+postName
-
-
-newPostPath = os.path.join( script_directory, 'content', 'post', postFolderName)
-
-os.mkdir(newPostPath)
-
-newPostFile = os.path.join(newPostPath, (postFileName+".md"))
-
 headerTemplate = '''
 +++
 title = 'postName'
@@ -36,19 +17,19 @@ tags = []
 +++
 '''
 
+## get post name from input
+postName = input('Name of Post\n')
+
+## process variables
+postFileName = postName.replace(" ", "_")
+postFolderName = today+"__"+postName
+newPostPath = os.path.join( script_directory, 'content', 'post', postFolderName)
+newPostFile = os.path.join(newPostPath, (postFileName+".md"))
 headerDate = headerTemplate.replace('currentDate', today )
 headerTitle =  headerDate.replace('postName', postName)
 headerOut = headerTitle
 
-
+## make output
+os.mkdir(newPostPath)
 with open(newPostFile, 'w', encoding='utf-8') as f:
     f.write(headerOut)
-
-## add date
-
-
-## Make new folder in content/poss  dir_postname
-
-
-## make index.md in content/post/folder
-
